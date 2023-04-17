08e2c4166161534ebc79d6d2d6741b295627bceb
haemil <haemil.haley@gmail.com>
2023-04-17 23:30:36 +0900

    구성 변경

--- string/2744.java
+++ string/2744.java
@@ -1,15 +1,5 @@
 class Main {
 
-    private static String solution2(String example) {
-        char[] result = example.toCharArray();
-        for (int i = 0; i < example.length(); i++) {
-            if ('a' <= result[i] && result[i] <= 'z')
-                result[i] = (char)('A' + result[i] - 'a');
-            else result[i] = (char)('a' + result[i] - 'A');
-        }
-        return String.valueOf(result);
-    }
-
     private static String solution1(String example) {
         String result = "";
         String[] splitString = example.split("");
@@ -24,8 +14,18 @@ class Main {
         return  result;
     }
 
+    private static String solution2(String example) {
+        char[] result = example.toCharArray();
+        for (int i = 0; i < example.length(); i++) {
+            if ('a' <= result[i] && result[i] <= 'z')
+                result[i] = (char)('A' + result[i] - 'a');
+            else result[i] = (char)('a' + result[i] - 'A');
+        }
+        return String.valueOf(result);
+    }
+
     public static void main(String[] args) {
         String example = "WornAnswer";
-        System.out.println(solution2(example));
+        System.out.println(solution1(example));
     }
 }
